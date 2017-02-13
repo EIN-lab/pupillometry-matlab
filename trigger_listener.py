afa2dd76c9e91751380f6c472844159b2c7eea63
Kim Ferrari <kim.ferrari@uzh.ch>
2017-02-13 08:51:07 +0100

    Update trigger_listener.py

--- trigger_listener.py
+++ trigger_listener.py
@@ -13,7 +13,7 @@ except RuntimeError:
 
 # connect data drive
 isMount = os.path.ismount('/home/pi/mnt/finc/')
-if not ismount:
+if not isMount:
     try:
         p = Popen('mount /home/pi/mnt/finc/')
     except RuntimeError:
