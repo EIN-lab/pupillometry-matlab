7536a62c396e64ef67f850ba024a8429b8ae4078
Kim Ferrari <kim.ferrari@uzh.ch>
2017-05-30 14:56:06 +0200

    latest working version

--- trigger_listener.py
+++ trigger_listener.py
@@ -1,4 +1,4 @@
-import itertools, sys, os
+import sys, os
 from subprocess import call
 import json
 import datetime
@@ -19,12 +19,15 @@ if not isMount:
     except RuntimeError:
         print("No internet connection!")
 
+# Set GPIO mode
+GPIO.setmode(GPIO.BOARD)
+
 # Predefined values
 channel = 11
 fname = 'params.json'
 
 def cam_trigger(channel):
-    print('Trigger detected on channel %s'%channel)
+    print('Trigger detected on channel %s\n'%channel)
 
     data = read_json(fname)
     #width = int(data["cam_settings"]["width"])
@@ -35,10 +38,11 @@ def cam_trigger(channel):
     duration = int(data["cam_settings"]["duration"])
     filepath = ''.join((data["paths"]["savepath"], prefix, data["paths"]["filename"]))
 
-    print('Recording started')
+    print('Start recording\n')
     camera.start_recording(filepath)
     camera.wait_recording(duration)
     camera.stop_recording()
+    
     print('Recording ended\n')
 
 def read_json(fname):
@@ -48,8 +52,6 @@ def read_json(fname):
 
     return data
 
-# Set up GPIO
-GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Create Camera object
@@ -57,19 +59,18 @@ camera = PiCamera()
 camera.rotation = 180
 camera.color_effects = (128,128)
 camera.framerate = 25
-camera.start_preview(alpha=192) # remove alpha=192 to remove transparency
-sleep(2) # Camera warm-up time
+camera.start_preview(alpha=128)
 
-print('Waiting for trigger ')
-spinner = itertools.cycle(['-', '/', '|', '\\']) # set up spinning "wheel"
+#GPIO.add_event_detect(channel, GPIO.RISING, callback=cam_trigger,bouncetime=2000)
+
+while True:
+    ch_trig = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=10)
+    if ch_trig is not None:
+        cam_trigger(ch_trig)
 try:
     while True:
-        ch_trig = GPIO.wait_for_edge(channel, GPIO.RISING,timeout=200)
-        sys.stdout.write(spinner.next())  # write the next character
-        sys.stdout.flush()                # flush stdout buffer (actual character display)
-        sys.stdout.write('\b')            # erase the last written char
-        if ch_trig is not None:
-            cam_trigger(ch_trig)
+        sleep(.2)
 except KeyboardInterrupt:
     camera.stop_preview()
     GPIO.cleanup()
+    sys.exit()
