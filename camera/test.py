cadcfdd101dd3031ad66f0a36c355c614f6e619c
Kim <kim.ferrari@uzh.ch>
2018-11-13 14:37:35 -0100

    working trigger

--- camera/test.py
+++ camera/test.py
@@ -2,20 +2,19 @@ import RPi.GPIO as GPIO
 import time as time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-
-GPIO.add_event_detect(40, GPIO.RISING)  # add rising edge detection on a channel
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 time.sleep(10) #wait for second RPi to be ready
+print('Armed')
 
 start = time.time()
 
 while True:
 
-    if GPIO.event_detected(40):
-
-        print('Button 1 pressed')
+    GPIO.wait_for_edge(40, GPIO.FALLING) #workaround for messy edge detection
+    time.sleep(0.005) #debounce 5ms
+    if GPIO.input(40) == 0:
+        print('Button pressed!')
 
     if time.time() - start > 30:
 
